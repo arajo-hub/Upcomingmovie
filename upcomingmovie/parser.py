efb249ea6d081d9fb1fbeaed9cc9b082916b40f2
arajo-hub <joara9566@naver.com>
2020-05-20 23:43:38 +0900

    modeling completed

--- upcomingmovie/parser.py
+++ upcomingmovie/parser.py
@@ -13,9 +13,10 @@ from parsed_data.models import parsed_movie
 
 def parse_movie():
     movie={}
+    codes=[]
     title=[]
-    code=[]
-    dateNcode=[]
+    opening_date=[]
+    TitleDate=[]
     valueList=[]
 
     response=urlopen('https://movie.naver.com/movie/running/premovie.nhn')
@@ -23,11 +24,10 @@ def parse_movie():
 
     for anchor in soup.select('li>dl>dt>a'):
         link=anchor['href']
-        code.append(link[link.index('=')+1:])
-        movie[anchor.get_text()]=''
+        code=link[link.index('=')+1:]
+        codes.append(code)
+        movie[code]=''
         title.append(anchor.get_text())
-
-    opening_date=[]
 
     for anchor in soup.select('li>dl>dd:nth-child(3)>dl>dd'):
         if "개봉" in anchor.get_text():
@@ -35,14 +35,14 @@ def parse_movie():
         else:
             pass
 
-    for i in range(len(code)):
-        dateNcode.append(code[i])
-        dateNcode.append(opening_date[i])
-        valueList.append(dateNcode)
-        dateNcode=[]
-
     for i in range(len(title)):
-        movie[title[i]]=valueList[i]
+        TitleDate.append(title[i])
+        TitleDate.append(opening_date[i])
+        valueList.append(TitleDate)
+        TitleDate=[]
+
+    for j in range(len(codes)):
+        movie[codes[j]]=valueList[j]
 
     return movie
 
@@ -51,5 +51,5 @@ cur.execute('SELECT * FROM parsed_data_parsed_movie')
 
 if __name__=='__main__':
     movie_dict=parse_movie()
-    for t, dNc in movie_dict.items():
-        parsed_movie(title=t, date=dNc[0], code=dNc[1]).save()
+    for key, value in movie_dict.items():
+        parsed_movie(title=value[0], date=value[1], code=key).save()
